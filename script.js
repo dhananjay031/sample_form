cfd1b2c7405efd550b1680d23a5e64c59e8bb5fb
Dhananjay <user@DhananjaysMac.local>
2023-09-06 20:31:27 +0530

    initial form

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleClick(e){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    console.log({
+        name,
+        email,
+        message
+    })
+    
+}
